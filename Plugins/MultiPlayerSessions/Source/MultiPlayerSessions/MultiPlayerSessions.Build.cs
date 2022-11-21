30d208e584b5e6f381699158b518329dd9318a2e
zhaozigu <zhaozigunan@163.com>
2022-11-21 23:32:47 +0800

    feat. [code] Session functioality

--- Plugins/MultiPlayerSessions/Source/MultiPlayerSessions/MultiPlayerSessions.Build.cs
+++ Plugins/MultiPlayerSessions/Source/MultiPlayerSessions/MultiPlayerSessions.Build.cs
@@ -26,6 +26,11 @@ public class MultiPlayerSessions : ModuleRules
 			new string[]
 			{
 				"Core",
+				"OnlineSubsystem",
+				"OnlineSubsystemSteam",
+				"UMG",
+				"Slate",
+				"SlateCore"
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
